9e12c663fabfe4408695dc3b71476d15b48f4323
Gabriel Soares <biel.soares.melo@gmail.com>
2018-03-20 21:21:10 -0300

    ESTE PROJETO FOI PASSADO PARA OUTRO REPOSITÓRIO.
    Mas deixo o código para quem quiser dica de como fazer uma "simulação" de orientação objeto em C puro.

--- CRUD-LP1.c
+++ CRUD-LP1.c
@@ -1,38 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef struct{
 	int floors, rooms;
 	float terrainArea, builtArea;
 }tCasa;
 
+
 typedef struct{
 	float area, condom;
 	int rooms, garage;
-	char posit[30];
+	char pos[30];
 }tApartamento;
+
 
 typedef struct{
 	float area;
 }tTerreno;
 
+
+typedef union{
+	tCasa c;
+	tApartamento a;
+	tTerreno t;
+}tUnion;
+
+
 typedef struct{
-	char streetName[50], hood[50], city[50];
+	char streetname[50], hood[50], city[50];
 	int number, cep, flag; // flag = 1 -> à venda // flag = 2 -> para alugar // flag = 3 -> os dois
 	float value;
-	tCasa house;
-	tApartamento ap;
-	tTerreno tr;
+	int type; // type = 1 -> casa // type = 2 -> apartamento // type = 3 -> terreno
+	tUnion d;
 }tImovel;
-
-int main(void){
-
-
-
-
-
-
-
-
-	return 0;
-}
